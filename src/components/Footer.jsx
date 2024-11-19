05476f30d3f8cc3be689f85709039bf515ebd397
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-19 20:57:18 +0800

    header and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Vaughn Fitz Benedicto</p>
+        </footer>
+    );
+}
+
+export default Footer
