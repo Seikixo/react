228e5d3bffb0e44618344fc690706814bd102b8a
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-24 18:56:53 +0800

    using filter metod

--- src/components/Drinks/Drinks.jsx
+++ src/components/Drinks/Drinks.jsx
@@ -7,19 +7,38 @@ function Drinks(){
         {id: 3, name: "Pepsi", calories: 150}, 
         {id: 4, name: "7Up", calories: 140}];
 
-    //sorting array using .sort method
-
+    //Sorting array using .sort method
     drinks.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetical Order
     //drinks.sort((a, b) => b.name.localeCompare(a.name)); Reverse Alphabetical Order
     //drinks.sort((a, b) => a.calories - b.calories); Asceding 
     // drinks.sort((a, b) => b.calories - a.calories); Descending
 
-    const listOfDrinks = drinks.map(drink => <li key={drink.id}> {drink.name}: &nbsp; {drink.calories} </li>);
+    //Filtering array using .filter method
+    const lowCalDrinks = drinks.filter(drink => drink.calories < 150);
+
+    //Creating another array using .map method
+    const listOfDrinks = drinks.map(drink => <li key={drink.id}> 
+                                                {drink.name}: 
+                                                &nbsp; {drink.calories} 
+                                            </li>);
+
+    const listOfLowCalDrinks = lowCalDrinks.map(lowCalDrink => <li key={lowCalDrink.id}>
+                                                                    {lowCalDrink.name}:
+                                                                    &nbsp; {lowCalDrink.calories}
+                                                                </li>)
 
     return(
-        <ul>
-            {listOfDrinks}
-        </ul>
+        <>
+            <ul>
+                {listOfDrinks}
+            </ul>
+
+            <p>Low Calories Drinks</p>
+            <ul>
+                {listOfLowCalDrinks}
+            </ul>
+        </>
+
     )
 }
 
