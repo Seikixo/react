9f71fac67535f51d4293f48d92b9f4d184fb1989
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-24 18:42:44 +0800

    using sort method

--- src/components/Drinks/Drinks.jsx
+++ src/components/Drinks/Drinks.jsx
@@ -7,6 +7,13 @@ function Drinks(){
         {id: 3, name: "Pepsi", calories: 150}, 
         {id: 4, name: "7Up", calories: 140}];
 
+    //sorting array using .sort method
+
+    drinks.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetical Order
+    //drinks.sort((a, b) => b.name.localeCompare(a.name)); Reverse Alphabetical Order
+    //drinks.sort((a, b) => a.calories - b.calories); Asceding 
+    // drinks.sort((a, b) => b.calories - a.calories); Descending
+
     const listOfDrinks = drinks.map(drink => <li key={drink.id}> {drink.name}: &nbsp; {drink.calories} </li>);
 
     return(
