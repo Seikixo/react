ba7ebc9937d7d9aa935420a8575d2bb50ba07abc
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-25 18:14:11 +0800

    handling click event with condition

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,24 @@
 
 function Button(){
-
+    let count = 0;
+    //call back function
     const handleClick1 = () => console.log("Yikes")
-
+    //call back function with parameter
     const handleClick2 = (name) => console.log(`${name} please stop`)
+    //call back function with condition
+    const handleClickCount = (name) => {
+        if(count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} times`)
+        }
+        else{
+            console.log(`${name} stop it`)
+        }
+    }
 
     //applying external style sheet to apply global style
     return(
-        <button onClick={() => handleClick2("Seikizo")} className="button">Click Me </button>
+        <button onClick={() => handleClickCount("Seikizo")} className="button">Click Me </button>
     );
 }
 
