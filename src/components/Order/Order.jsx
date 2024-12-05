2310a77fc323e31e1185e11b44546105eeae9334
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-12-05 10:35:56 +0800

    handling onChange

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -6,6 +6,7 @@ function Order(){
     const [order, setOrder] = useState("None");
     const [pieces, setPieces] = useState(0);
     const [submit, setSubmit] = useState(false);
+    const [payment, setPayment] = useState();
 
     const updateOrder = () => {
         setOrder("apple");
@@ -20,6 +21,10 @@ function Order(){
         setSubmit(!submit);
     }
 
+    function handlePayment(event){
+        setPayment(event.target.value);
+    }
+
     return(
         <>
             <p>Name: {order}</p>
@@ -27,6 +32,14 @@ function Order(){
 
             <p>Pieces: {pieces}</p>
             <button onClick={updatePieces}>Add Pieces</button>
+
+            <p>Payment Type: {payment}</p>
+            <select value={payment} onChange={handlePayment}>
+                <option value="">Select Option</option>
+                <option value="Visa">Visa</option>
+                <option value="MasterCard">MasterCard</option>
+                <option value="GiftCard">GiftCard</option>
+            </select>
 
             <p>Submit: {submit ? 'False' : 'True'}</p>
             <button onClick={updateSubmit}>Submit</button>
