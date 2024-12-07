61dd526f65e29e8bbb8eade485a73bb9d6209e2b
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-12-07 14:36:21 +0800

    using useRef()

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,24 +1,28 @@
+import React, {useEffect,useState,useRef} from "react";
+
 
 function Button(){
-    let count = 0;
+    const clickRef = useRef(0);
     //call back function
     const handleClick1 = () => console.log("Yikes")
     //call back function with parameter
     const handleClick2 = (name) => console.log(`${name} please stop`)
     //call back function with condition
-    const handleClickCount = (name) => {
-        if(count < 3){
-            count++;
-            console.log(`${name} you clicked me ${count} times`)
-        }
-        else{
-            console.log(`${name} stop it`)
-        }
-    }
 
+    useEffect(() => {
+        console.log("Button render");
+    })
+
+    function handleClickCount(){
+        clickRef.current++;
+        console.log(clickRef.current);
+    }
     //applying external style sheet to apply global style
     return(
-        <button onClick={() => handleClickCount("Seikizo")} className="button">Click Me </button>
+        <>
+            <button onClick={handleClickCount} className="button">Click Me </button>
+        </>
+
     );
 }
 
